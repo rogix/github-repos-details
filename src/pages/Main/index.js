792b127d9dc83a7dd4578c6b3f6fb1f02c966cb1
Roger <12768403+rogix@users.noreply.github.com>
2019-08-22 21:48:35 -0300

    chores: fix some types

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -4,20 +4,20 @@ import { Container, GithubSearch, Form, SubmitButton } from './styles';
 import SearchIcon from '../../assets/images/search-icon.svg';
 
 export default function Main() {
-	return (
-		<Container>
-			<GithubSearch>
-				<strong>Github</strong>
-				<span>Search</span>
-			</GithubSearch>
+  return (
+    <Container>
+      <GithubSearch>
+        <strong>Github</strong>
+        <span>Search</span>
+      </GithubSearch>
 
-			<Form>
-				<input type="text" />
+      <Form>
+        <input type="text" />
 
-				<SubmitButton>
-					<img src={SearchIcon} alt="search" />
-				</SubmitButton>
-			</Form>
-		</Container>
-	);
+        <SubmitButton>
+          <img src={SearchIcon} alt="search" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
